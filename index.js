1fcbad0ca51f36e13e1317b501217f810d76aecd
Voom Developer <alkhwlanyhsyn199@gmail.com>
2026-09-09 22:47:29 +0300

    إضافة أزرار الإعجاب والحفظ

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ main{padding-bottom:85px}
 <div class="card" onclick="openVideo()">
 <div class="thumb">🎬</div>
 <div class="title">أول فيديو في منصة Voom</div>
-<div class="channel">Voom Gaming</div>
+<div class="channel">Voom Gaming</div><button>👍 إعجاب</button> <button>🔖 حفظ</button>
 </div>
 
 <div class="card" onclick="openVideo()">
