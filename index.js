beb1d9ae95092823b70b451f4f60c9dfd4c7397a
Voom Developer <alkhwlanyhsyn199@gmail.com>
2026-09-10 14:55:01 +0300

    إضافة زر الإشعارات

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ main{padding-bottom:85px}
 
 <header>
 <div class="logo">Voom</div>
-<div class="search" onclick="searchVoom()">⌕</div><div class="search" onclick="alert('لا توجد إشعارات جديدة 🔔')">🔔</div>
+<div class="search" onclick="searchVoom()">⌕</div><div class="search" onclick="alert('🔔 لا توجد إشعارات جديدة')">🔔</div><div class="search" onclick="alert('لا توجد إشعارات جديدة 🔔')">🔔</div>
 </header>
 
 <main>
